738b83015663b9380dc13bbc500c4a441a753161
vkozub <vsevolod.kozub@gmail.com>
2021-08-14 10:09:23 -0400

    First setup of allure cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -30,3 +30,27 @@ Capybara.default_selector = :css
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, { browser: :chrome, options: options })
 end
+
+require 'allure-cucumber'
+# require 'allure-ruby-commons'
+# include AllureCucumber
+
+# AllureCucumber.configure do |config|
+#   config.results_directory = "report/allure-results"
+#   config.clean_results_directory = true
+#   config.logging_level = Logger::INFO
+#   config.logger = Logger.new($stdout, Logger::DEBUG)
+#   config.environment = "staging"
+#
+#   # these are used for creating links to bugs or test cases where {} is replaced with keys of relevant items
+#   config.link_tms_pattern = "http://www.jira.com/browse/{}"
+#   config.link_issue_pattern = "http://www.jira.com/browse/{}"
+#
+#   # additional metadata
+#   # environment.properties
+#   config.environment_properties = {
+#     custom_attribute: "foo"
+#   }
+#   # categories.json
+#   config.categories = File.new("my_custom_categories.json")
+# end
