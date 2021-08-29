2829a09b8d9efb50f4b0ebf4924b1f437ccd617b
vkozub <vsevolod.kozub@gmail.com>
2021-08-29 05:07:15 -0400

    Add logger gem

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,7 @@
+require 'allure-cucumber'
+require 'capybara'
 require 'capybara/cucumber'
+require 'cucumber'
 require 'dotenv'
 require 'erb'
 require 'ffaker'
@@ -32,29 +35,25 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, { browser: :chrome, options: options })
 end
 
-require 'allure-cucumber'
-# require 'allure-ruby-commons'
-# include AllureCucumber
+AllureCucumber.configure do |config|
+  config.results_directory = "report/allure-results"
+  config.clean_results_directory = true
+  # config.logging_level = Logger::INFO
+  # config.logger = Logger.new($stdout, Logger::DEBUG)
+  # config.environment = "staging"
 
-# AllureCucumber.configure do |config|
-#   config.results_directory = "report/allure-results"
-#   config.clean_results_directory = true
-#   config.logging_level = Logger::INFO
-#   config.logger = Logger.new($stdout, Logger::DEBUG)
-#   config.environment = "staging"
-#
-#   # these are used for creating links to bugs or test cases where {} is replaced with keys of relevant items
-#   config.link_tms_pattern = "http://www.jira.com/browse/{}"
-#   config.link_issue_pattern = "http://www.jira.com/browse/{}"
-#
-#   # additional metadata
-#   # environment.properties
-#   config.environment_properties = {
-#     custom_attribute: "foo"
-#   }
-#   # categories.json
-#   config.categories = File.new("my_custom_categories.json")
-# end
+  # these are used for creating links to bugs or test cases where {} is replaced with keys of relevant items
+  config.link_tms_pattern = "http://www.jira.com/browse/{}"
+  config.link_issue_pattern = "http://www.jira.com/browse/{}"
+
+  # additional metadata
+  # environment.properties
+  config.environment_properties = {
+    custom_attribute: "foo"
+  }
+  # categories.json
+  config.categories = File.new("my_custom_categories.json")
+end
 
 FileUtils.mkdir_p('report/cucumber-reports/html') unless File.directory?('report/cucumber-reports/html')
 FileUtils.mkdir_p('report/cucumber-reports/rerun') unless File.directory?('report/cucumber-reports/rerun')
